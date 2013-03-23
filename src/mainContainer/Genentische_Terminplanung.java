b539a1227fb0e85d87945ce2e6bd2d9d6891d846
MarkusBl <markus.bless@htwg-konstanz.de>
2013-03-23 13:35:44 +0100

    added declarations

--- src/mainContainer/Genentische_Terminplanung.java
+++ src/mainContainer/Genentische_Terminplanung.java
@@ -2,12 +2,37 @@ package mainContainer;
 
 public class Genentische_Terminplanung {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/************* Globale Definitionen ********************************/
+	static final int MAXLEB = 12;		/* Anzahl der Lebewesen  */
+	static final int  S_TAGPUNKTE = 3;  /* Strafpunkte für Prüfungen am selben Tag */
+	static final int N_TAGPUNKTE = 1;   /* Strafpunkte für Prüfungen am nächsten Tag */
+	
+	/************* Globale Deklarationen ********************************/
 
+	/*int (*pool)[MAXLEB] 	/* Zeiger auf ein Array mit MAXLEB Integerelementen
+							siehe Funktion eingeben
+							Gesamtheit aller Lebewesen + Feld für Strafpunkte */
+		   					/**Achtung: Durch Umdrehung von MAXLEB und MAXGEN erscheint es 
+		   					teilweise etwas unlogisch. Rein gefühlsmäßig würde
+	           				man Spalten und Zeilen nicht wie oben anordnen **/
+		//(*paar), siehe Funktion eingeben
+							// Feld für die 0/1 - Kombination für die Paarung
+		int genera, 		// aktuelle Zahl der Generationen
+		bestleb,			// Index des besten Lebewesens
+		partner,
+		worstleb1,      	// Index des schlechtesten Lebewesens
+		worstleb2,      	// Index des 2. schlechtesten Lebewesens
+		anz_pruef,      	// Anzahl der Prüfungen die verteilt werden sollen
+		maxgen;				// Maximale Anzahl der Gene => Anzahl der Prüfungen
+		long maxgenera;		// maximal erlaubte Zahl der Generationen
+	
+	public static void main(String[] args) {
+		
 	}
+	
+	public void init(){
+		
+	}
+	
 
 }
